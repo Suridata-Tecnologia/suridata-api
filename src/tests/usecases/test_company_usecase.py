0b1a2e4e5af2bc4a47bb0f3e5204ba497d4d5aee
r0bfp <robfpinheiro@gmail.com>
2024-03-27 08:41:57 -0300

    test: add test to company usecase

---
+++ src/tests/usecases/test_company_usecase.py
@@ -0,0 +1,37 @@
+from unittest.mock import MagicMock, Mock
+from sqlalchemy.orm import Session
+
+from src.schemas.company import Company
+from src.repositories.company import CompanyRepository
+from src.usecases.company import CompanyUseCase
+
+
+def should_return_list_of_companies_when_call_method():
+    mock_db = MagicMock(spec=Session)
+    mock_company_1 = Company(
+        id=1,
+        status=1,
+        pretty_name="Company 1",
+        name="Company1",
+        drive_path="DRIVE_TESTE",
+        policy="1234s",
+        suridata_product='Test Product',
+        dashboard_param='1234asdc'
+    )
+    mock_company_2 = Company(
+        id=2,
+        status=1,
+        pretty_name="Company 1",
+        name="Company1",
+        drive_path="DRIVE_TESTE",
+        policy="1234s",
+        suridata_product='Test Product',
+        dashboard_param='1234asdc'
+    )
+    mock_companies = [mock_company_1, mock_company_2]
+
+    CompanyRepository.find_all = Mock(return_value=mock_companies)
+
+    result = CompanyUseCase.list_companies(mock_db)
+
+    assert result == mock_companies
